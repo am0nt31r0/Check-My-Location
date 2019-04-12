4a0c13154b762f48bc05659418609a27e073f6cc
André Monteiro <36992115+am0nt31r0@users.noreply.github.com>
2019-04-12 14:52:49 +0100

    Update location.py

--- location.py
+++ location.py
@@ -1,14 +1,24 @@
 #!/usr/bin/python3
 
-from urllib.request import urlopen
+import urllib.request
+import urllib.error
 import json
 
-key=""
-pub_ip = urlopen("http://ipecho.net/plain").read().decode('utf-8')
-data = urlopen("http://api.ipstack.com/" + str(pub_ip) + "?access_key=" + key).read().decode('utf-8')
+try:
+    urllib.request.urlopen("https://ipecho.net/plain", timeout = 1)
 
-j = json.loads(data) 
-print ("IP: " + j['ip'])
-print ("CONTINENT: " + j['continent_name'])
-print ("COUNTRY/CITY: " + j['country_name'] + " - " + j['city'] + " (" + j['zip'] + ")")
-print ("LATITUDE: " + str(j['latitude']) + " LONGITUDE: " + str(j['longitude']))
+except urllib.error.URLError as e:
+
+    print ('Network connection is down...')
+
+else:
+    
+    key="" # insert your API key here
+    pub_ip = urllib.request.urlopen("http://ipecho.net/plain").read().decode('utf-8')
+    data = urllib.request.urlopen("http://api.ipstack.com/" + str(pub_ip) + "?access_key=" + key).read().decode('utf-8')
+
+    j = json.loads(data)
+    print ("[*] IP: " + j['ip'])
+    print ("[*] CONTINENT: " + j['continent_name'])
+    print ("[*] COUNTRY/CITY: " + j['country_name'] + " - " + j['city'] + " (" + j['zip'] + ")")
+    print ("[*] LATITUDE: " + str(j['latitude']) + " LONGITUDE: " + str(j['longitude']))
