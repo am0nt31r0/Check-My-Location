4f4289fb309e3add05962cb6c720eea95e8f603b
André Monteiro <36992115+am0nt31r0@users.noreply.github.com>
2019-04-12 15:40:44 +0100

    Update location.py

--- location.py
+++ location.py
@@ -5,16 +5,9 @@ import urllib.error
 import json
 
 try:
-    urllib.request.urlopen("https://ipecho.net/plain", timeout = 1)
-
-except urllib.error.URLError as e:
-
-    print ('Network connection is down...')
-
-else:
+    pub_ip = urllib.request.urlopen("https://ipecho.net/plain", timeout = 1).read().decode('utf-8')
     
     key="" # insert your API key here
-    pub_ip = urllib.request.urlopen("http://ipecho.net/plain").read().decode('utf-8')
     data = urllib.request.urlopen("http://api.ipstack.com/" + str(pub_ip) + "?access_key=" + key).read().decode('utf-8')
 
     j = json.loads(data)
@@ -22,3 +15,9 @@ else:
     print ("[*] CONTINENT: " + j['continent_name'])
     print ("[*] COUNTRY/CITY: " + j['country_name'] + " - " + j['city'] + " (" + j['zip'] + ")")
     print ("[*] LATITUDE: " + str(j['latitude']) + " LONGITUDE: " + str(j['longitude']))
+
+except urllib.error.URLError as e:
+
+    print ('Network connection is down...')
+    
+    
